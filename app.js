1200a2d174099845d0444f904657fdcf54b40e10
Kamlesh Chandnani <kamlesh@Kamlesh-2.local>
2017-09-09 12:07:45 +0530

    Fixed issue: By default all the splitted modules were loaded

--- app.js
+++ app.js
@@ -1,51 +1,45 @@
+import React from "react";
+import { render } from "react-dom";
+import { Router, browserHistory, match } from "react-router";
 
-import React from 'react';
-import { render } from 'react-dom';
-import { Router, browserHistory, match } from 'react-router';
+import Root from "./Root";
 
-import Root from './Root';
-
-// const asyncComponent = bundle => (nextState, callback) => {
-// 	bundle.then(component => callback(null, component.default ? component.default : component));
-// };
-
-const loadRoute = cb => {
-    return (module) => cb(null, module.default);
-}
+const asyncComponent = bundle => (nextState, callback) => {
+  bundle().then(component => {
+    callback(null, component.default ? component.default : component);
+  });
+};
 
 const routes = {
-    path: '/',
-    component: Root,
-    indexRoute: {
-    getComponent(location, cb){
-            System.import(/* webpackChunkName: "Home", webpackMode: "lazy" */ './Home.js')
-            .then(loadRoute(cb))
-            .catch(errorLoading);
-        }
+  path: "/",
+  component: Root,
+  indexRoute: {
+    getComponent: asyncComponent(() =>
+      import(/* webpackChunkName: "Home", webpackMode: "lazy" */ "./Home.js")
+    )
+  },
+  childRoutes: [
+    {
+      path: "/page-a",
+      getComponent: asyncComponent(() =>
+        import(/* webpackChunkName: "PageA", webpackMode: "lazy" */ "./PageA.js")
+      )
     },
-    childRoutes: [
-        {
-            path: '/page-a',
-            getComponent(location, cb){
-                System.import(/* webpackChunkName: "PageA", webpackMode: "lazy" */ './PageA.js')
-                .then(loadRoute(cb))
-                .catch(errorLoading);
-            }
-        },
-        {
-            path: '/page-b',
-            getComponent(location, cb){
-                System.import(/* webpackChunkName: "PageB", webpackMode: "lazy" */ './PageB.js')
-                .then(loadRoute(cb))
-                .catch(errorLoading);
-            }
-        }
-    ]
-}
+    {
+      path: "/page-b",
+      getComponent: asyncComponent(() =>
+        import(/* webpackChunkName: "PageB", webpackMode: "lazy" */ "./PageB.js")
+      )
+    }
+  ]
+};
 
-match({ history: browserHistory, routes }, (error, redirectLocation, renderProps) => { //eslint-disable-line
-	render(
-        <Router {...renderProps} history={browserHistory}>{routes}</Router>,
-        document.getElementById('container')
-	);
+match({ history: browserHistory, routes }, (error, redirectLocation, renderProps) => {
+  //eslint-disable-line
+  render(
+    <Router {...renderProps} history={browserHistory}>
+      {routes}
+    </Router>,
+    document.getElementById("container")
+  );
 });
